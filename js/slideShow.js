288b5df6c5e13191c22150eec3795d9f818ecd9b
RobBotelho <35775014+RobBotelho@users.noreply.github.com>
2018-02-12 13:34:56 -0800

    Update slideShow.js

--- js/slideShow.js
+++ js/slideShow.js
@@ -1,119 +1,63 @@
+var current = 0;
+var playing = 1;
+var timeReturn;
+var prev = 3;
+timer();
+function timer() {
+    timeReturn = setInterval(function() {
+        prev = current;
+        current = (++current) % 4; // next
+        
+        $('#slide' + prev).fadeOut(2000);
+        $('#slide' + current).fadeIn(2000);
+    }, 5000);
+}
+$(document).on('click','.fa-pause',function(){
+    $(this).removeClass('fa-pause');
+   $(this).addClass('fa-play');
+   playing = 0;
+   clearInterval(timeReturn);
 
-            var url = [
-            	'images/2017/Group-Photo-2017.jpg',
-            	 'images/data/Bacteria-fragmentation.png',
-            	'images/data/super.jpg',
-              'images/data/merge-epi.jpg'
-            ]
-            var time;
-            var index = 0;
-            var currentOption;
-            Timer();
-            $('.slide a').click(function(){
-              var options = ($(this).attr('id'));
-              currentOption = options;
-              startTime();
+ })
+ $(document).on('click','.fa-play',function(){
+    $(this).removeClass('fa-play');
+   $(this).addClass('fa-pause');
+   playing = 1;
+   timer();
+  
 
-     	      $('.slide a').not(options).each(function(){
-	              $(this).children().removeClass('fa fa-circle');
-	              $(this).children().addClass('fa fa-circle-o');
-              });
-              $('#'+options).children().addClass('fa fa-circle');
-              
-              $('.fade').fadeIn(500);
-              $('#header0').children().fadeOut(500);
-              setTimeout(function(){
-              	 
-              	 if (options.toString() == 'circleOne'){
-                   var background = $('#main > section.one');
-              	 	background.css('background-image', 'url('+url[0]+')');
-                    background.css('background-size', 'cover');
-                   
-              	 	$('#header-1').fadeIn();
-              	 	index = 0;
+ })
+ $('.fa-forward').click(function(){
+    clearInterval(timeReturn);
+    prev = current;
+    current = (++current) % 4; // next  
+    $('#slide' + prev).fadeOut();
+    $('#slide' + current).fadeIn();
 
-              	 } else if (options == 'circleTwo'){
-                   var background = $('#main > section.one');
-             	  	 background.css('background-image', 'url('+url[1]+')');
-                  background.css('background-size', 'cover');
-             	 	 $('#header-2').fadeIn();
-              	 	 index = 1;
-
-              	 } else if (options =='circleThree'){
-                   var background = $('#main > section.one');
-              		 background.css('background-image', 'url('+url[2]+')');
-                    background.css('background-size', 'cover');
-              		 $('#header-3').fadeIn();
-              	 	index = 2;
-
-
-              	 } 
-                  else if (options =='circleFour'){
-                   var background = $('#main > section.one');
-                   background.css('background-size', 'contain');
-                   background.css('background-image', 'url('+url[3]+')');
-                   background.css('background-repeat', 'no-repeat');
-                   
-                   
-                   $('#header-4').fadeIn();
-                  index = 3;
-
-
-                 } 
-
-            	 $('.fade').fadeOut(500);
-
-              },500);
-            });
-            function Timer (){
-            	time = setInterval(function(){
-            	$('.fade').fadeIn(500);
-            	$('#header0').children().fadeOut(500);
-            	 index = (++index % 4);
-            	 setTimeout(function(){
-            	 	var option;
-                var background = $('#main > section.one')
-            	 	background.css('background-image', 'url('+url[index]+')');
-
-            	 	if (index == 0){
-            	 		option = 'circleOne';
-                   background.css('background-size', 'cover');
-            	 		$('#header-1').fadeIn(1500);
+    if (playing){
+        timer();
+    }
+    
+ });
+  $('.fa-backward').click(function(){
+    clearInterval(timeReturn);
+    
+    $('#slide' + prev).fadeIn();
+    $('#slide' + current).fadeOut();
+    current = prev;
+    if (prev == 0){
+      prev = 3;
+    } else {
+        prev = --prev;
+    }
+    
+    if (playing){
+      timer();
+    } 
+    
+ });
 
 
-            	 	} else if (index == 1){
-            	 		option = 'circleTwo';
-                   background.css('background-size', 'cover');
-            	 		$('#header-2').fadeIn(1500);
-
-            	 	} else if (index == 2) {
-            	 		option = 'circleThree';
-                   background.css('background-size', 'cover');
-                  
-            	 		$('#header-3').fadeIn(1500);
 
 
-            	 	}
-                else {
-                  option = 'circleFour';
-                    background.css('background-size', 'contain');
-                   background.css('background-repeat', 'no-repeat');
-                  $('#header-4').fadeIn();
 
-
-                }
-	        	 	  $('.slide a').not(option).each(function(){
-			              $(this).children().removeClass('fa fa-circle');
-			              $(this).children().addClass('fa fa-circle-o');
-		              });
-	         		  $('#'+option).children().addClass('fa fa-circle');
-
-            	 	$('.fade').fadeOut(500);
-            	 },500);
-            }, 8000);
-            }
-            function startTime(){
-            	clearInterval(time);
-            	Timer();
-            }
-   
